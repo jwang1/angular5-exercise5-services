a5d0b1a50790100719ef9c0605529b6d4253981e
jwang1 <sigkdd@yahoo.com>
2018-02-19 22:09:43 -0500

    minor updates; need to double check it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,21 @@
-import {Component, DoCheck, OnInit} from '@angular/core';
-import {User} from './shared/user.model';
-import {UsersService} from './shared/users.service';
+import { Component, DoCheck } from '@angular/core';
+import { UsersService } from './shared/users.service';
 
+/**
+  note the UserService is referred in app.module.ts, for all components.
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [UsersService]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, DoCheck {
+export class AppComponent implements DoCheck {
   title = 'Angular 5 Services Practice';
 
   constructor(private userService: UsersService) {}
 
   activatedCnt = 0;
   deactivatedCnt = 0;
-
-  activeUsers: User[];
-  inactiveUsers: User[];
-
-  ngOnInit(): void {
-    this.activeUsers = this.userService.activeUsers;
-    this.inactiveUsers = this.userService.inactiveUsers;
-  }
 
   ngDoCheck(): void {
     this.activatedCnt = this.userService.activatedCnt;
